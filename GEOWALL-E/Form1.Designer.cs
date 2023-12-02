038506f79eb967df4befd6fc92b9be4edbb3cbb6
abelponce03 <129076568+abelponce03@users.noreply.github.com>
2023-12-02 18:02:47 -0500

    Actualizacion de interfaz

--- GEOWALL-E/Form1.Designer.cs
+++ GEOWALL-E/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GEOWALL_E
 {
-    partial class Form1
+    partial class GEOWALL_E
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,17 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GEOWALL_E));
             BTN_ACCION = new Button();
             PANEL_COMANDOS = new TextBox();
+            PANEL_DIBUJO = new PictureBox();
+            IMPORTAR = new Button();
+            GUARDADR_FUNCIONES = new Button();
+            IMAGEN_WALL_E_NOMBRE = new Label();
+            WALLE_E_NOMBRE = new Label();
+            IMAGEN_WALLE = new Label();
+            ((System.ComponentModel.ISupportInitialize)PANEL_DIBUJO).BeginInit();
             SuspendLayout();
             // 
             // BTN_ACCION
             // 
-            BTN_ACCION.Location = new Point(671, 384);
+            BTN_ACCION.Location = new Point(359, 671);
             BTN_ACCION.Name = "BTN_ACCION";
-            BTN_ACCION.Size = new Size(115, 50);
+            BTN_ACCION.Size = new Size(180, 86);
             BTN_ACCION.TabIndex = 0;
-            BTN_ACCION.Text = "Evaluar";
+            BTN_ACCION.Text = "DIBUJAR";
             BTN_ACCION.UseVisualStyleBackColor = true;
             BTN_ACCION.Click += BTN_ACCION_Click;
             // 
@@ -46,23 +54,90 @@
             // 
             PANEL_COMANDOS.BackColor = SystemColors.HighlightText;
             PANEL_COMANDOS.Cursor = Cursors.IBeam;
-            PANEL_COMANDOS.Dock = DockStyle.Left;
-            PANEL_COMANDOS.Location = new Point(0, 0);
+            PANEL_COMANDOS.Location = new Point(31, 94);
             PANEL_COMANDOS.Multiline = true;
             PANEL_COMANDOS.Name = "PANEL_COMANDOS";
-            PANEL_COMANDOS.Size = new Size(224, 450);
+            PANEL_COMANDOS.Size = new Size(575, 563);
             PANEL_COMANDOS.TabIndex = 2;
             PANEL_COMANDOS.TextChanged += PANEL_COMANDOS_TextChanged;
             // 
-            // Form1
+            // PANEL_DIBUJO
+            // 
+            PANEL_DIBUJO.BackColor = SystemColors.ButtonHighlight;
+            PANEL_DIBUJO.Location = new Point(634, 51);
+            PANEL_DIBUJO.Name = "PANEL_DIBUJO";
+            PANEL_DIBUJO.Size = new Size(1236, 890);
+            PANEL_DIBUJO.TabIndex = 3;
+            PANEL_DIBUJO.TabStop = false;
+            PANEL_DIBUJO.Click += PANEL_DIBUJO_Click;
+            // 
+            // IMPORTAR
+            // 
+            IMPORTAR.Location = new Point(359, 763);
+            IMPORTAR.Name = "IMPORTAR";
+            IMPORTAR.Size = new Size(180, 88);
+            IMPORTAR.TabIndex = 4;
+            IMPORTAR.Text = "IMPORTAR";
+            IMPORTAR.UseVisualStyleBackColor = true;
+            IMPORTAR.Click += IMPORTAR_Click;
+            // 
+            // GUARDADR_FUNCIONES
+            // 
+            GUARDADR_FUNCIONES.Location = new Point(359, 857);
+            GUARDADR_FUNCIONES.Name = "GUARDADR_FUNCIONES";
+            GUARDADR_FUNCIONES.Size = new Size(180, 84);
+            GUARDADR_FUNCIONES.TabIndex = 5;
+            GUARDADR_FUNCIONES.Text = "GUARDAR";
+            GUARDADR_FUNCIONES.UseVisualStyleBackColor = true;
+            GUARDADR_FUNCIONES.Click += GUARDADR_FUNCIONES_Click;
+            // 
+            // IMAGEN_WALL_E_NOMBRE
+            // 
+            IMAGEN_WALL_E_NOMBRE.AutoSize = true;
+            IMAGEN_WALL_E_NOMBRE.Image = (Image)resources.GetObject("IMAGEN_WALL_E_NOMBRE.Image");
+            IMAGEN_WALL_E_NOMBRE.ImageAlign = ContentAlignment.TopCenter;
+            IMAGEN_WALL_E_NOMBRE.Location = new Point(1017, 28);
+            IMAGEN_WALL_E_NOMBRE.Name = "IMAGEN_WALL_E_NOMBRE";
+            IMAGEN_WALL_E_NOMBRE.Size = new Size(0, 20);
+            IMAGEN_WALL_E_NOMBRE.TabIndex = 6;
+            // 
+            // WALLE_E_NOMBRE
+            // 
+            WALLE_E_NOMBRE.BackColor = SystemColors.ActiveCaptionText;
+            WALLE_E_NOMBRE.BorderStyle = BorderStyle.FixedSingle;
+            WALLE_E_NOMBRE.ForeColor = SystemColors.ControlText;
+            WALLE_E_NOMBRE.Image = Properties.Resources.walle_nombre;
+            WALLE_E_NOMBRE.Location = new Point(104, 9);
+            WALLE_E_NOMBRE.Name = "WALLE_E_NOMBRE";
+            WALLE_E_NOMBRE.Size = new Size(416, 63);
+            WALLE_E_NOMBRE.TabIndex = 7;
+            // 
+            // IMAGEN_WALLE
+            // 
+            IMAGEN_WALLE.Image = Properties.Resources.images;
+            IMAGEN_WALLE.Location = new Point(83, 674);
+            IMAGEN_WALLE.Name = "IMAGEN_WALLE";
+            IMAGEN_WALLE.Size = new Size(192, 270);
+            IMAGEN_WALLE.TabIndex = 8;
+            // 
+            // GEOWALL_E
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ControlText;
+            ClientSize = new Size(1882, 953);
+            Controls.Add(IMAGEN_WALLE);
+            Controls.Add(WALLE_E_NOMBRE);
+            Controls.Add(IMAGEN_WALL_E_NOMBRE);
+            Controls.Add(GUARDADR_FUNCIONES);
+            Controls.Add(IMPORTAR);
+            Controls.Add(PANEL_DIBUJO);
             Controls.Add(PANEL_COMANDOS);
             Controls.Add(BTN_ACCION);
-            Name = "Form1";
-            Text = "Form1";
+            Name = "GEOWALL_E";
+            Text = "GEOWALL-E";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)PANEL_DIBUJO).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -71,5 +146,11 @@
 
         private Button BTN_ACCION;
         private TextBox PANEL_COMANDOS;
+        private PictureBox PANEL_DIBUJO;
+        private Button IMPORTAR;
+        private Button GUARDADR_FUNCIONES;
+        private Label IMAGEN_WALL_E_NOMBRE;
+        private Label WALLE_E_NOMBRE;
+        private Label IMAGEN_WALLE;
     }
 }
--- GEOWALL-E/Form1.cs
+++ GEOWALL-E/Form1.cs
@@ -1,18 +1,33 @@
 
 namespace GEOWALL_E
 {
-    public partial class Form1 : Form
+    public partial class GEOWALL_E : Form
     {
-        public Form1()
+        public GEOWALL_E()
         {
             InitializeComponent();
         }
 
         private void BTN_ACCION_Click(object sender, EventArgs h)
         {
-            
+
         }
         private void PANEL_COMANDOS_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PANEL_DIBUJO_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IMPORTAR_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GUARDADR_FUNCIONES_Click(object sender, EventArgs e)
         {
 
         }
