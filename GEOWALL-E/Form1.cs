6947455f45644b2c2b61cedcd5b76cd45d8bc614
abelponce03 <129076568+abelponce03@users.noreply.github.com>
2023-12-02 16:54:04 -0500

    bases de la aplicacion

---
+++ GEOWALL-E/Form1.cs
@@ -0,0 +1,20 @@
+
+namespace GEOWALL_E
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_ACCION_Click(object sender, EventArgs h)
+        {
+            
+        }
+        private void PANEL_COMANDOS_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
